493a34bdca2b695ac9565964978b5ecf0aeb18e4
xwills1337 <misha.sid@rambler.ru>
2023-05-14 16:52:08 +0400

    Создал класс бинарного дерева. Добавил конструктор по умолчанию и конструктор копирования.

--- AISD_Laba_21/Source.cpp
+++ AISD_Laba_21/Source.cpp
@@ -3,6 +3,34 @@
 #include<conio.h>
 #include<vector>
 
+class binary_tree
+{
+public:
+    struct Node
+    {
+        int key;
+        Node* left;
+        Node* right;
+        Node(int k) : key(k), left(NULL), right(NULL) {}
+    };
+    
+    binary_tree() : root(NULL) {}
+    binary_tree(const binary_tree& m) : root(copy(m.root)) {}
+
+private:
+    Node* root;
+    Node* copy(Node* node) 
+    {
+        if (!node) return NULL;
+        Node* new_node = new Node(node->key);
+        new_node->left = copy(node->left);
+        new_node->right = copy(node->right);
+        return new_node;
+    }
+};
+
+
+
 int main()
 {
 
